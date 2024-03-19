cf286c677fc3cb2134f0706045ff360ceeb1c46c
Akbar Wijayanto <akbar.wijayanto94@gmail.com>
2024-03-19 22:55:10 +0700

    update column role name to enum

---
+++ src/db/enum.ts
@@ -0,0 +1,5 @@
+export enum RoleEnum {
+  ADMIN = 'ADMIN',
+  OPERATION = 'OPERATION',
+  CUSTOMER = 'CUSTOMER',
+}
--- src/roles/entities/roles.entity.ts
+++ src/roles/entities/roles.entity.ts
@@ -1,4 +1,5 @@
 import { BaseModel } from 'src/common/base.model.entity'
+import { RoleEnum } from 'src/db/enum'
 import { Users } from 'src/users/entities/users.entity'
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
 
@@ -7,8 +8,8 @@ export class Roles extends BaseModel {
   @PrimaryGeneratedColumn()
   id: number
 
-  @Column('varchar')
-  name: string
+  @Column({ type: 'enum', enum: RoleEnum, nullable: true })
+  name: RoleEnum
 
   @OneToMany(() => Users, (user) => user.role)
   users: Users[]
