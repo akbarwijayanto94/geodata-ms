ffd191da9a9bd6ef758f5f4f7967341228ac9905
Akbar Wijayanto <akbar.wijayanto94@gmail.com>
2024-03-20 15:07:09 +0700

    add geodata endpoint and validation

--- src/modules/geodata/dto/create-geodata.dto.ts
+++ src/modules/geodata/dto/create-geodata.dto.ts
@@ -1,13 +1,28 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { IsString } from 'class-validator'
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
+import { IsOptional, IsString } from 'class-validator'
 import { GeoJsonTypeMemberEnum } from 'src/db/enum'
 
 export class CreateGeoDataDto {
   @IsString()
   @ApiProperty({
     type: String,
+    description: 'Title of data',
+  })
+  title: string
+
+  @IsString()
+  @IsOptional()
+  @ApiPropertyOptional({
+    type: String,
+    description: 'Description of data',
+  })
+  description?: string
+
+  @IsString()
+  @ApiProperty({
+    type: String,
     enum: GeoJsonTypeMemberEnum,
     description: 'Geo Type Member',
   })
-  type: string
+  type: GeoJsonTypeMemberEnum
 }
--- src/modules/geodata/geodata.service.ts
+++ src/modules/geodata/geodata.service.ts
@@ -1,11 +1,54 @@
 import { Injectable } from '@nestjs/common'
+import * as fs from 'fs'
+import * as GV from 'geo-valley'
+import { join } from 'path'
+import { renderFullName } from 'src/common/utils/string.util'
+import { GeoJsonTypeMemberEnum } from 'src/db/enum'
 import { Users } from '../users/entities/users.entity'
 import { CreateGeoDataDto } from './dto/create-geodata.dto'
+import GeodataRepository from './geodata.repository'
 
 @Injectable()
 export class GeodataService {
-  async create(dto: CreateGeoDataDto, user: Users, geoFiles?: Express.Multer.File[]) {
-    // TODO store data to DB
-    return geoFiles
+  constructor(private readonly repository: GeodataRepository) {}
+
+  async create(dto: CreateGeoDataDto, user: Users, file?: Express.Multer.File) {
+    const data = fs.readFileSync(join(process.cwd(), file.path)).toString()
+    const jsonData = JSON.parse(data)
+
+    const validationResponse = await this.validateGeoData(dto.type, jsonData)
+
+    if (!validationResponse) {
+      const result = await this.repository.save({
+        title: dto.title,
+        description: dto.description,
+        type: dto.type,
+        data: jsonData,
+        createdBy: renderFullName(user),
+      })
+
+      return result
+    }
+
+    return validationResponse
+  }
+
+  async validateGeoData(type: GeoJsonTypeMemberEnum, jsonData: any) {
+    switch (type) {
+      case GeoJsonTypeMemberEnum.POINT:
+        return await GV.isPoint(jsonData)
+      case GeoJsonTypeMemberEnum.MULTI_POINT:
+        return await GV.isMultiPoint(jsonData)
+      case GeoJsonTypeMemberEnum.LINE_STRING:
+        return await GV.isLineString(jsonData)
+      case GeoJsonTypeMemberEnum.MULTI_LINE_STRING:
+        return await GV.isMultiLineString(jsonData)
+      case GeoJsonTypeMemberEnum.POLYGON:
+        return await GV.isPolygon(jsonData)
+      case GeoJsonTypeMemberEnum.MULTI_POLYGON:
+        return await GV.isMultiPolygon(jsonData)
+      default:
+        return
+    }
   }
 }
