371bde71dc21f7fd1bdcacb200170b3ef845735c
Akbar Wijayanto <akbar.wijayanto94@gmail.com>
2024-03-21 09:10:26 +0700

    add description property

--- src/modules/users/dto/login.dto.ts
+++ src/modules/users/dto/login.dto.ts
@@ -6,6 +6,7 @@ export class LoginDto {
   @IsNotEmpty()
   @ApiProperty({
     type: String,
+    description: 'Email address',
   })
   email: string
 
@@ -13,6 +14,7 @@ export class LoginDto {
   @IsNotEmpty()
   @ApiProperty({
     type: String,
+    description: 'Password',
   })
   password: string
 }
